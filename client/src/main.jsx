ba1c0506452b605dbc2a4059a201394de4acd67e
Souvik Nag <s.nag26@gmail.com>
2024-02-18 11:52:09 +0530

    new client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
-
-import "./index.css";
-import { store } from "./store.js";
-import { Provider } from "react-redux";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <Provider store={store}>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-    <ToastContainer position="top-right" />
-  </Provider>
-  // </React.StrictMode>
-);
+  </React.StrictMode>,
+)
